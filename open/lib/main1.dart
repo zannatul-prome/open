02279568422add00298d1183dd29820d04ed30bd
zannatul-prome <zannatulprome20@gmail.com>
2025-07-30 11:57:25 +0600

    up

--- open/lib/main1.dart
+++ open/lib/main1.dart
@@ -1,3 +1,30 @@
-void main(){
-  
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body:Gradientcolor()
+      ),
+    ),
+  );
+}
+
+
+
+ class Gradientcolor extends StatelessWidget{
+  const Gradientcolor({super.key});
+  Widget build (context){
+    return  Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.purple, Colors.cyan]),
+          ),
+          child: Center(
+            child: Text(
+              "hi Moral Values",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+  }
 }
